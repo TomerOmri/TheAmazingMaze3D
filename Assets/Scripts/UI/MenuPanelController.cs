20a1bd08170f79be68edf401dda4d5b76e503ed3
tomeromri <tomer92@gmail.com>
2017-08-25 10:07:44 +0300

    Added menu and clicking features

---
+++ Assets/Scripts/UI/MenuPanelController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanelController : MonoBehaviour {
+
+	public void showMainMenu(){
+		gameObject.SetActive (true);
+	}
+
+	public void hideMainMenu(){
+		gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/UI/howToPlayController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class howToPlayController : MonoBehaviour {
+
+	public void showHowToPlay(){
+		gameObject.SetActive (true);
+	}
+
+	public void hideHowToPlay(){
+		gameObject.SetActive (false);
+	}
+}
